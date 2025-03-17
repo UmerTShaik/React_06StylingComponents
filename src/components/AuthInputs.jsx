b188c3eae351d63f06917cc37575f0f11ded675d
Umer <umer.tariq.shaik@softrams.com>
2025-03-17 11:58:57 -0400

    first commit without tialwind

---
+++ src/components/AuthInputs.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import {styled} from 'styled-components';
+
+import Button from './Button.jsx';
+import Input from './Input.jsx';
+
+const ControlContainer = styled.div` 
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+  `//tagged template js feature
+
+
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === 'email') {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      {/* <div className="controls"> */}
+      <ControlContainer>
+        {/* <p className="paragraph"> Lesson:117, since we are importing CustomInput
+          <Label $invalid = {emailNotValid} >Email</Label> */}
+          <Input label="Email" invalid = {emailNotValid}
+            type="email"
+            // className={emailNotValid ? 'invalid' : undefined}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+           />
+         {/* </p> */}
+        {/* <p>
+          <Label $invalid={passwordNotValid}>Password</Label> */}
+          <Input invalid={passwordNotValid} label="Password"
+            type="password"
+            // className={passwordNotValid ? 'invalid' : undefined}
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+          />
+        {/* </p> */}
+        </ControlContainer>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
